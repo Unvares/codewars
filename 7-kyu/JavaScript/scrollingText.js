616fd03d42bf2d3525e4091be3d68a25e82b4ae5
unvares <okmanis8@gmail.com>
2022-01-21 10:42:57 +0500

    21.01.2022: 7-kyu x3

---
+++ 7-kyu/JavaScript/scrollingText.js
@@ -0,0 +1,35 @@
+/* 
+
+  Your task is to complete the function which takes a string,
+  and returns an array with all possible rotations of the given
+  string, in uppercase.
+
+  Example
+   scrollingText("codewars") should return:
+    [ "CODEWARS",
+      "ODEWARSC",
+      "DEWARSCO",
+      "EWARSCOD",
+      "WARSCODE",
+      "ARSCODEW"
+      "RSCODEWA",
+      "SCODEWAR" ]
+    Good luck!
+
+  https://www.codewars.com/kata/5a995c2aba1bb57f660001fd
+
+  21.01.2022.
+
+*/
+
+function scrollingText(text){
+  let result = [];
+  
+  for(let i = 0; i < text.length; i++) {
+    result.push(
+      (text.slice(i) + text.slice(0, i)).toUpperCase()
+    );
+  }
+  
+  return result;
+}
---
+++ 7-kyu/JavaScript/stringMerge.js
@@ -0,0 +1,25 @@
+/* 
+
+  Given two words and a letter, return a single word that's
+  a combination of both words, merged at the point where the
+  given letter first appears in each word. The returned word
+  should have the beginning of the first word and the ending
+  of the second, with the dividing letter in the middle.
+  You can assume both words will contain the dividing letter.
+
+  Examples
+    ("hello", "world", "l")       ==>  "held"
+    ("coding", "anywhere", "n")   ==>  "codinywhere"
+    ("jason", "samson", "s")      ==>  "jasamson"
+    ("wonderful", "people", "e")  ==>  "wondeople"
+
+  https://www.codewars.com/kata/597bb84522bc93b71e00007e
+
+  21.01.2022.
+
+*/
+
+function stringMerge(s1, s2, l){
+  return s1.slice(0, s1.indexOf(l) )
+    + s2.slice( s2.indexOf(l) );
+}
