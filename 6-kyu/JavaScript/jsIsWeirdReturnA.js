dbdbe3b2373b0f121e7df1f8c2fe3ffd1eb4ddc9
unvares <okmanis8@gmail.com>
2022-04-25 13:09:52 +0300

    25.04.2022: 6-kyu x1

---
+++ 6-kyu/JavaScript/jsIsWeirdReturnA.js
@@ -0,0 +1,12 @@
+/*
+
+  There are only two tests:
+  1) Assign "a" to variable x
+  2) Only use the characters below:
+     +()[]!x=
+
+  https://www.codewars.com/kata/57f90a8d5cae44a9dc000091
+
+*/
+
+x=([]+![])[+!![]]
