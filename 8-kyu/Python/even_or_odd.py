c61ea1672abee5d83a0bd25e07cd478e6392f8e6
unvares <okmanis8@gmail.com>
2022-10-04 21:11:33 +0200

    04.10.2022: 8-kyu x1, 6-kyu x2

---
+++ 8-kyu/Python/even_or_odd.py
@@ -0,0 +1,10 @@
+"""
+Create a function that takes an integer as an argument and returns "Even" for
+even numbers or "Odd" for odd numbers.
+
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+"""
+
+
+def even_or_odd(n):
+    return 'Even' if n % 2 == 0 else 'Odd'
